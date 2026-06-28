f131f75fee1f56699de7a8cad3e2a3dfbccc93e2
Sai Sharan Patil <167446525+saisharanpati1@users.noreply.github.com>
2026-06-28 14:45:31 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(ch=='('){
+                stack.push(')');
+            }
+             else if(ch=='['){
+                stack.push(']');
+            }
+             else if(ch=='{'){
+                stack.push('}');
+            }
+            else{
+                if(stack.isEmpty() || ch!=stack.pop()){
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
+}
